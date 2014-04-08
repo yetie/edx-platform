e64fda0f7ca2d4e70d17c415aa6ae7b198af248c
Valera Rozuvan <valera@edx.org>
2014-04-08 23:27:43 +0000

    Tweaking automatic screenshots in acceptance tests.

--- common/djangoapps/terrain/browser.py
+++ common/djangoapps/terrain/browser.py
@@ -244,10 +244,10 @@ def capture_screenshot_for_step(step, when):
         world.capture_screenshot("image_name")
     """
     if world.auto_capture_screenshots:
-        scenario_num = step.scenario.feature.scenarios.index(step.scenario)
+        scenario_num = step.scenario.feature.scenarios.index(step.scenario) + 1
         step_num = step.scenario.steps.index(step) + 1
         step_func_name = step.defined_at.function.func_name
-        image_name = "{prefix:03d}__{num}__{name}__{postfix}".format(
+        image_name = "{prefix:03d}__{num:03d}__{name}__{postfix}".format(
             prefix=scenario_num,
             num=step_num,
             name=step_func_name,
